92a4e1c1878e92bb6961539f38381086f54f8edc
João Victor Dias <joaovictor665@gmail.com>
2020-09-19 00:17:34 -0300

    .

--- calculadora/main.cpp
+++ calculadora/main.cpp
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 using namespace std;
 
-double Calc(int n, int x, int y){
+
+
+
+
+double Calc(int n, double x , double y){
     if(n == 1){
         return x + y;
     }if (n == 2)
@@ -14,10 +18,12 @@ double Calc(int n, int x, int y){
     }if (n == 4)
     {
         return x/y;
-    }if (n == 5)
+    }else
     {
-        return x % y;
-    }    
+        return 0 ;
+    }
+    
+     
 }
 
 int  main(){
@@ -27,13 +33,34 @@ int  main(){
     cin>>x;
     cout<<"escolha outro numero"<<endl;
     cin>>y;
-    cout<<"escolha uma das operacoes abaixo:"<<endl
+    do
+    {
+        cout<<"escolha uma das operacoes abaixo:"<<endl
         <<"1: somar"<<endl
         <<"2: subtrair"<<endl
         <<"3: multiplicar"<<endl
         <<"4: dividir"<<endl
-        <<"5: resto da divisao"<<endl;
-    cin>>n;
-    cout<<Calc(n,x,y)<<endl;
+        <<"5 para sair"<<endl;
+        
+        cin>>n;
+
+        if(n ==1 || n==2 || n==3 || n==4 || n == 5){
+            if(Calc(n,x,y)==0){
+                cout<<"tchau"<<endl;
+            }else
+            {
+                cout<<Calc(n,x,y)<<endl;
+                
+            }
+            
+        }else
+        {
+            cout<<"numero invalido"<<endl;
+        }
+            
+        
+
+    } while (n!=5);
+    return 0;
     system("pause");
 }
